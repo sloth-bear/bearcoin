b6aa8082b34dace304c6207957a7337895ddffe1
sloth-bear <slothbear.hj@gmail.com>
2022-01-31 20:06:43 +0900

    Add a CLI command

--- main.go
+++ main.go
@@ -1,11 +1,38 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/sloth-bear/bearcoin/explorer"
-	"github.com/sloth-bear/bearcoin/rest"
 )
 
+func usages() {
+	fmt.Printf("Welcome to bearcoin!\n\n")
+	fmt.Printf("Please use the following commands:\n\n")
+	fmt.Printf("explorer:   Start the HTML Explorer\n")
+	fmt.Printf("rest:	    Start the REST API (recommended)\n\n")
+
+	os.Exit(0)
+}
+
 func main() {
-	go explorer.Start(3000)
-	rest.Start(4000)
+	args := os.Args
+
+	if len(args) < 2 {
+		usages()
+	}
+
+	switch args[1] {
+	case "explorer":
+		fmt.Println("Start Explorer")
+		explorer.Start(3000)
+
+	case "rest":
+		fmt.Println("Start REST API")
+		explorer.Start(4000)
+
+	default:
+		usages()
+	}
 }
