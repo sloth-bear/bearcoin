4588effbec340ea21a8b0ede1870814ed4c03bb7
sloth-bear <slothbear.hj@gmail.com>
2022-01-31 20:54:01 +0900

    Refactor command line interface

---
+++ cli/cli.go
@@ -0,0 +1,37 @@
+package cli
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/sloth-bear/bearcoin/explorer"
+	"github.com/sloth-bear/bearcoin/rest"
+)
+
+func usages() {
+	fmt.Printf("Welcome to bearcoin!\n\n")
+	fmt.Printf("You can use the following flags:\n\n")
+	fmt.Printf("-mode:    Choose between 'html' and 'rest' (default: rest)\n")
+	fmt.Printf("-port:    Set port of the server (default: 4000)\n\n")
+}
+
+func Start() {
+	if len(os.Args) == 1 {
+		usages()
+	}
+
+	mode := flag.String("mode", "rest", "Choose between 'html' and 'rest'")
+	port := flag.Int("port", 4000, "Sets port of the server")
+
+	flag.Parse()
+
+	switch *mode {
+	case "html":
+		explorer.Start(*port)
+	case "rest":
+		rest.Start(*port)
+	default:
+		usages()
+	}
+}
--- main.go
+++ main.go
@@ -1,38 +1,7 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-	"os"
-)
-
-func usages() {
-	fmt.Printf("Welcome to bearcoin!\n\n")
-	fmt.Printf("Please use the following flags:\n\n")
-	fmt.Printf("-mode=rest: Choose between 'html' and 'rest'\n")
-	fmt.Printf("-port=4000 Set port of the server\n\n")
-
-	os.Exit(0)
-}
-
-var mode string
-var port int
+import "github.com/sloth-bear/bearcoin/cli"
 
 func main() {
-	flag.StringVar(&mode, "mode", "rest", "Choose between 'html' and 'rest'")
-	flag.IntVar(&port, "port", 4000, "Sets port of the server")
-	flag.Parse()
-
-	if !flag.Parsed() {
-		os.Exit(0)
-	}
-
-	switch mode {
-	case "html":
-		fmt.Println("Start explorer")
-	case "rest":
-		fmt.Println("Start REST API")
-	default:
-		usages()
-	}
+	cli.Start()
 }
