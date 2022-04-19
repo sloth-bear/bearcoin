d967dc3ad02b19425ebed139d2c2cd43fd77b032
slothbear <slothbear.hj@gmail.com>
2022-04-19 23:10:07 +0900

    Add balance API

--- rest/rest.go
+++ rest/rest.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/sloth-bear/bearcoin/blockchain"
+	"github.com/sloth-bear/bearcoin/utils"
 )
 
 var port string
@@ -26,6 +27,11 @@ type urlDescription struct {
 	Payload     string `json:"payload,omitempty"`
 }
 
+type balanceResponse struct {
+	Address string `json:"address"`
+	Balance int    `json:"balance"`
+}
+
 type errorResponse struct {
 	Message string `json:"message"`
 }
@@ -37,6 +43,7 @@ func documentation(rw http.ResponseWriter, r *http.Request) {
 		{URL: url("/blocks"), Method: "POST", Description: "Add A Block"},
 		{URL: url("/blocks"), Method: "GET", Description: "See All Blocks"},
 		{URL: url("/blocks/{hash}"), Method: "GET", Description: "See A Block"},
+		{URL: url("/balance/{address}"), Method: "GET", Description: "Get TxOuts for an Address"},
 	}
 
 	json.NewEncoder(rw).Encode(data)
@@ -77,15 +84,29 @@ func status(rw http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(rw).Encode(blockchain.Blockchain())
 }
 
+func balance(rw http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	address := vars["address"]
+	total := r.URL.Query().Get("total")
+	switch total {
+	case "true":
+		amount := blockchain.Blockchain().BalanceByAddress(address)
+		json.NewEncoder(rw).Encode(balanceResponse{address, amount})
+	default:
+		utils.HandleErr(json.NewEncoder(rw).Encode(blockchain.Blockchain().TxOutsByAddress(address)))
+	}
+}
+
 func Start(aPort int) {
-	handler := mux.NewRouter()
-	handler.HandleFunc("/", documentation).Methods("GET")
-	handler.HandleFunc("/status", status).Methods("GET")
-	handler.HandleFunc("/blocks", blocks).Methods("GET", "POST")
-	handler.HandleFunc("/blocks/{hash:[a-f0-9]+}", block).Methods("GET")
-	handler.Use(jsonContentTypeMiddleware)
+	router := mux.NewRouter()
+	router.HandleFunc("/", documentation).Methods("GET")
+	router.HandleFunc("/status", status).Methods("GET")
+	router.HandleFunc("/blocks", blocks).Methods("GET", "POST")
+	router.HandleFunc("/blocks/{hash:[a-f0-9]+}", block).Methods("GET")
+	router.HandleFunc("/balance/{address}", balance).Methods("GET")
+	router.Use(jsonContentTypeMiddleware)
 
 	port = fmt.Sprintf(":%d", aPort)
 	fmt.Printf("Listening on http://localhost%s\n", port)
-	log.Fatal(http.ListenAndServe(port, handler))
+	log.Fatal(http.ListenAndServe(port, router))
 }
