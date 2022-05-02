6ecf3efc8ae4aaaf9f84870029d61031868ff2b9
slothbear <slothbear.hj@gmail.com>
2022-05-02 23:12:58 +0900

    Add getting to upgrade request port

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -3,6 +3,7 @@ package p2p
 import (
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/gorilla/websocket"
 	"github.com/sloth-bear/bearcoin/utils"
@@ -17,11 +18,13 @@ func Upgrade(rw http.ResponseWriter, r *http.Request) {
 
 	conn, err := upgrader.Upgrade(rw, r, nil)
 	utils.HandleErr(err)
-	initPeer(conn, "xx", "xx")
+	result := strings.Split(r.RemoteAddr, ":")
+	port := strings.TrimPrefix(r.URL.Query().Get("openPort"), ":")
+	initPeer(conn, result[0], port)
 }
 
-func AddPeer(address, port string) {
-	url := fmt.Sprintf("ws://%s:%s/ws", address, port)
+func AddPeer(address, port, openPort string) {
+	url := fmt.Sprintf("ws://%s:%s/ws?openPort=%s", address, port, openPort)
 	conn, _, err := websocket.DefaultDialer.Dial(url, nil)
 	utils.HandleErr(err)
 	initPeer(conn, address, port)
