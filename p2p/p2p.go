52c254a02bd990872312640a659bf53df9ee80d3
slothbear <slothbear.hj@gmail.com>
2022-05-09 00:41:22 +0900

    Add close function
    - Add to close connection, channel
    - Remove unused codes

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -3,7 +3,6 @@ package p2p
 import (
 	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/gorilla/websocket"
 	"github.com/sloth-bear/bearcoin/utils"
@@ -22,19 +21,13 @@ func Upgrade(rw http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(rw, r, nil)
 	utils.HandleErr(err)
 
-	peer := initPeer(conn, ip, port)
-
-	time.Sleep(20 * time.Second)
-	peer.inbox <- []byte("Hello from Port 3000!")
+	initPeer(conn, ip, port)
 }
 
 func AddPeer(address, port, openPort string) {
 	url := fmt.Sprintf("ws://%s:%s/ws?openPort=%s", address, port[0:], openPort)
 	conn, _, err := websocket.DefaultDialer.Dial(url, nil)
 	utils.HandleErr(err)
-	
-	peer := initPeer(conn, address, port)
 
-	time.Sleep(10 * time.Second)
-	peer.inbox <- []byte("Hello from 4000!")
+	initPeer(conn, address, port)
 }
