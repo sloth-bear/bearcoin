f3427ad769ed53e16770d553ef82cfc3b3a51a49
slothbear <slothbear.hj@gmail.com>
2022-06-06 19:47:15 +0900

    Add utils.go test codes

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -2,7 +2,9 @@ package utils
 
 import (
 	"encoding/hex"
+	"encoding/json"
 	"fmt"
+	"reflect"
 	"testing"
 )
 
@@ -32,4 +34,69 @@ func ExampleHash() {
 	x := Hash(s)
 	fmt.Println(x)
 	// Output: e005c1d727f7776a57a661d61a182816d8953c0432780beeae35e337830b1746
+}
+
+func TestToBytes(t *testing.T) {
+	s := "test"
+	b := ToBytes(s)
+
+	k := reflect.TypeOf(b).Kind()
+	if k != reflect.Slice {
+		t.Errorf("ToBytes should return a slice of bytes, but got %s", k)
+	}
+}
+
+func TestSplitter(t *testing.T) {
+	type test struct {
+		input  string
+		sep    string
+		index  int
+		output string
+	}
+
+	tests := []test{
+		{input: "0:1:2", sep: ":", index: 1, output: "1"},
+		{input: "0:1:2", sep: ":", index: 10, output: ""},
+		{input: "0:1:2", sep: "none", index: 1, output: ""},
+	}
+
+	for _, tc := range tests {
+		got := Splitter(tc.input, tc.sep, tc.index)
+		if got != tc.output {
+			t.Errorf("Expected: %s, got: %s", tc.output, got)
+		}
+	}
+}
+
+func TestFromBytes(t *testing.T) {
+	type testStruct struct {
+		Test string
+	}
+
+	var restored testStruct
+	ts := testStruct{"test"}
+	b := ToBytes(ts)
+
+	FromBytes(&restored, b)
+	if !reflect.DeepEqual(ts, restored) {
+		t.Error("FromBytes() should restore struct")
+	}
+}
+
+func TestJsonToBytes(t *testing.T) {
+	type testStruct struct{ Test string }
+
+	s := testStruct{"test"}
+	b := JsonToBytes(s)
+
+	k := reflect.TypeOf(b).Kind()
+	if k != reflect.Slice {
+		t.Errorf("Expected %v and got %v", reflect.Slice, k)
+	}
+
+	var restored testStruct
+	json.Unmarshal(b, &restored)
+	if !reflect.DeepEqual(s, restored) {
+		t.Error("JsonToBytes() should encode to JSON correctly.")
+	}
 }
