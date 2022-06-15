ccee206eb3479eebe51e5cdea0ac496203615660
slothbear <slothbear.hj@gmail.com>
2022-06-15 21:55:38 +0900

    Add Blockchain() test

--- blockchain/chain_test.go
+++ blockchain/chain_test.go
@@ -1,8 +1,15 @@
 package blockchain
 
+import (
+	"sync"
+	"testing"
+
+	"github.com/sloth-bear/bearcoin/utils"
+)
+
 type fakeDB struct {
-	fakeLocadChain func() []byte
-	fakeFindBlock  func(hash string) []byte
+	fakeLoadChain func() []byte
+	fakeFindBlock func(hash string) []byte
 }
 
 func (f fakeDB) FindBlock(hash string) []byte {
@@ -16,5 +23,35 @@ func (fakeDB) DeleteAllBlocks() {}
 func (fakeDB) SaveChain(blockchain []byte) {}
 
 func (f fakeDB) LoadChain() []byte {
-	return f.fakeLocadChain()
+	return f.fakeLoadChain()
+}
+
+func TestBlockchain(t *testing.T) {
+	t.Run("Should create blockchain if not exists", func(t *testing.T) {
+		dbStorage = fakeDB{
+			fakeLoadChain: func() []byte {
+				return nil
+			},
+		}
+
+		bc := Blockchain()
+		if bc.Height != 1 {
+			t.Error("Blockchain() should create a blockchain")
+		}
+	})
+
+	t.Run("Should restore blockchain if exists", func(t *testing.T) {
+		once = *new(sync.Once)
+		dbStorage = fakeDB{
+			fakeLoadChain: func() []byte {
+				bc := &blockchain{Height: 1, NewestHash: "NewestHash", CurrentDifficulty: 1}
+				return utils.ToBytes(bc)
+			},
+		}
+
+		bc := Blockchain()
+		if bc.NewestHash != "NewestHash" {
+			t.Errorf("Blockchain() should restore a blockchain with a newestHash of %s, god %s", "NewestHash", bc.NewestHash)
+		}
+	})
 }
